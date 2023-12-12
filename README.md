d29102d45e5ec60d3228e0354539151c42c090b8
Bram Borggreve <beeman@beeman.nl>
2023-12-12 18:41:42 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# create-solana-dapp-deployer
---
+++ generate-templates.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Define workspace and base variables
+WORKSPACE_ROOT="${WORKSPACE_ROOT:-$(pwd)/workspace}"
+BASE_PREFIX="solana-dapp-"
+BASE_REPO="https://github.com/beeman/"
+BASE_COMMAND="npx --yes create-solana-dapp@next"
+
+# Function to get template ID
+get_template_id() {
+    local preset=$1
+    local ui=$2
+    local id="${BASE_PREFIX}${preset}"
+    if [[ "$ui" != "none" ]]; then
+        id="${id}-${ui}"
+    fi
+    echo "$id"
+}
+
+# Array of templates
+declare -a presets=("next" "react")
+declare -a uis=("tailwind" "none")
+
+# Create workspace directory if it doesn't exist
+if [[ ! -d "$WORKSPACE_ROOT" ]]; then
+    echo "Creating workspace directory... $WORKSPACE_ROOT"
+    mkdir -p "$WORKSPACE_ROOT"
+fi
+
+# Iterate over templates
+for preset in "${presets[@]}"; do
+    for ui in "${uis[@]}"; do
+        id=$(get_template_id "$preset" "$ui")
+        repo="${BASE_REPO}${id}"
+        target_dir="${WORKSPACE_ROOT}/${id}"
+        command="${BASE_COMMAND} ${id} --preset ${preset} --ui ${ui} --anchor counter --yarn"
+
+        # Check if target directory exists
+        if [[ ! -d "$target_dir" ]]; then
+            echo "Generating template... $id in $WORKSPACE_ROOT, command:"
+            echo "    $command"
+            echo "    Target: $target_dir"
+            (cd "$WORKSPACE_ROOT" && eval "$command")
+
+            # Add, push, and remove git remote
+            echo "Adding remote... $repo $target_dir"
+            (cd "$target_dir" && git remote add origin "$repo")
+
+            echo "Pushing template... $id to $repo"
+            (cd "$target_dir" && git push -u origin main --force)
+
+            echo "Removing remote... $repo $target_dir"
+            (cd "$target_dir" && git remote remove origin)
+        else
+            echo "Skipping template... $id, already exists in $WORKSPACE_ROOT"
+        fi
+    done
+done
+
+echo "Done!"
